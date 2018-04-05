5e9521cbd9fcd844197637f19c774f5634e9a9d2
ace19-dev <acemc19@gmail.com>
2018-04-05 04:46:47 -0400

    update unet

--- nets/unet.py
+++ nets/unet.py
@@ -90,11 +90,53 @@ def _upconv_2D(tensor, n_filter, flags, name):
         name="upsample_{}".format(name))
 
 
-def Unet(X, training, flags=None):
+def Unet_512(X, training, flags=None):  # 512 x 512
     """Build a U-Net architecture
 
     Args:
         X (4-D Tensor): (N, H, W, C)
+        training (1-D Tensor): Boolean Tensor is required for batchnormalization layers
+
+    Returns:
+        output (4-D Tensor): (N, H, W, C) - 512 x 512
+            Same shape as the `input` tensor
+
+    Notes:
+        U-Net: Convolutional Networks for Biomedical Image Segmentation
+        https://arxiv.org/abs/1505.04597
+    """
+    # net = X / 127.5 - 1
+    conv1, pool1 = _conv_conv_pool(X, [64, 64], training, flags, name=1)
+    conv2, pool2 = _conv_conv_pool(pool1, [128, 128], training, flags, name=2)
+    conv3, pool3 = _conv_conv_pool(pool2, [256, 256], training, flags, name=3)
+    conv4, pool4 = _conv_conv_pool(pool3, [512, 512], training, flags, name=4)
+    conv5 = _conv_conv_pool(pool4, [1024, 1024], training, flags, name=5, pool=False)
+
+    up6 = _upconv_concat(conv5, conv4, 512, flags, name=6)
+    conv6 = _conv_conv_pool(up6, [512, 512], training, flags, name=6, pool=False)
+
+    up7 = _upconv_concat(conv6, conv3, 256, flags, name=7)
+    conv7 = _conv_conv_pool(up7, [256, 256], training, flags, name=7, pool=False)
+
+    up8 = _upconv_concat(conv7, conv2, 128, flags, name=8)
+    conv8 = _conv_conv_pool(up8, [128, 128], training, flags, name=8, pool=False)
+
+    up9 = _upconv_concat(conv8, conv1, 64, flags, name=9)
+    conv9 = _conv_conv_pool(up9, [64, 64], training, flags, name=9, pool=False)
+
+    return tf.layers.conv2d(
+        conv9,
+        1, (1, 1),
+        name='final',
+        activation=tf.nn.sigmoid,
+        padding='same')
+
+
+def Unet_256(X, training, flags=None): # 256 x 256
+    """Build a U-Net architecture
+
+    Args:
+        X (4-D Tensor): (N, H, W, C) - 256 x 256
         training (1-D Tensor): Boolean Tensor is required for batchnormalization layers
 
     Returns:
@@ -106,88 +148,27 @@ def Unet(X, training, flags=None):
         https://arxiv.org/abs/1505.04597
     """
     # net = X / 127.5 - 1
-    conv1, pool1 = _conv_conv_pool(X, [8, 8], training, flags, name=1)
-    conv2, pool2 = _conv_conv_pool(pool1, [16, 16], training, flags, name=2)
-    conv3, pool3 = _conv_conv_pool(pool2, [32, 32], training, flags, name=3)
-    conv4, pool4 = _conv_conv_pool(pool3, [64, 64], training, flags, name=4)
-    conv5, pool5 = _conv_conv_pool(pool4, [128, 128], training, flags, name=5)
-    conv6, pool6 = _conv_conv_pool(pool5, [256, 256], training, flags, name=6)
-    conv7 = _conv_conv_pool(pool6, [512, 512], training, flags, name=7, pool=False)
+    conv1, pool1 = _conv_conv_pool(X, [32, 32], training, flags, name=1)
+    conv2, pool2 = _conv_conv_pool(pool1, [64, 64], training, flags, name=2)
+    conv3, pool3 = _conv_conv_pool(pool2, [128, 128], training, flags, name=3)
+    conv4, pool4 = _conv_conv_pool(pool3, [256, 256], training, flags, name=4)
+    conv5 = _conv_conv_pool(pool4, [512, 512], training, flags, name=5, pool=False)
 
-    up8 = _upconv_concat(conv7, conv6, 256, flags, name=8)
-    conv8 = _conv_conv_pool(up8, [256, 256], training, flags, name=8, pool=False)
+    up6 = _upconv_concat(conv5, conv4, 256, flags, name=6)
+    conv6 = _conv_conv_pool(up6, [256, 256], training, flags, name=6, pool=False)
 
-    up9 = _upconv_concat(conv8, conv5, 128, flags, name=9)
-    conv9 = _conv_conv_pool(up9, [128, 128], training, flags, name=9, pool=False)
+    up7 = _upconv_concat(conv6, conv3, 128, flags, name=7)
+    conv7 = _conv_conv_pool(up7, [128, 128], training, flags, name=7, pool=False)
 
-    up10 = _upconv_concat(conv9, conv4, 64, flags, name=10)
-    conv10 = _conv_conv_pool(up10, [64, 64], training, flags, name=10, pool=False)
+    up8 = _upconv_concat(conv7, conv2, 64, flags, name=8)
+    conv8 = _conv_conv_pool(up8, [64, 64], training, flags, name=8, pool=False)
 
-    up11 = _upconv_concat(conv10, conv3, 32, flags, name=11)
-    conv11 = _conv_conv_pool(up11, [32, 32], training, flags, name=11, pool=False)
-
-    up12 = _upconv_concat(conv11, conv2, 16, flags, name=12)
-    conv12 = _conv_conv_pool(up12, [16, 16], training, flags, name=12, pool=False)
-
-    up13 = _upconv_concat(conv12, conv1, 8, flags, name=13)
-    conv13 = _conv_conv_pool(up13, [8, 8], training, flags, name=13, pool=False)
+    up9 = _upconv_concat(conv8, conv1, 32, flags, name=9)
+    conv9 = _conv_conv_pool(up9, [32, 32], training, flags, name=9, pool=False)
 
     return tf.layers.conv2d(
-        conv13,
+        conv9,
         1, (1, 1),
         name='final',
         activation=tf.nn.sigmoid,
         padding='same')
-
-
-def Unet2(X, training, flags=None):
-    conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(X)
-    conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-
-    conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
-    conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-
-    conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
-    conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-
-    conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
-    conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
-    drop4 = Dropout(0.5)(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
-
-    conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
-    conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
-    drop5 = Dropout(0.5)(conv5)
-
-    up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-        UpSampling2D(size=(2, 2))(drop5))
-    merge6 = merge([drop4, up6], mode='concat', concat_axis=3)
-    conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
-    conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
-
-    up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-        UpSampling2D(size=(2, 2))(conv6))
-    merge7 = merge([conv3, up7], mode='concat', concat_axis=3)
-    conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
-    conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
-
-    up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-        UpSampling2D(size=(2, 2))(conv7))
-    merge8 = merge([conv2, up8], mode='concat', concat_axis=3)
-    conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
-    conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
-
-    up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-        UpSampling2D(size=(2, 2))(conv8))
-    merge9 = merge([conv1, up9], mode='concat', concat_axis=3)
-    conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
-    conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
-    conv9 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
-
-    conv10 = Conv2D(1, 1, activation='sigmoid')(conv9)
-
-    return conv10
-
