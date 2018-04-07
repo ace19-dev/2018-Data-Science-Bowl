b4042721f1464e8e652e09f73cc82a9f41b9baff
SungIl-Kim <joongjum@naver.com>
2018-04-08 03:13:50 +0900

    change elastic_trans.py

--- aug/elastic_tran.py
+++ aug/elastic_tran.py
@@ -1,92 +1,101 @@
-# Import stuff
 import os
 import sys
-import numpy as np
+import argparse
+import tqdm
 import cv2
 import uuid
+
+import numpy as np
+import tensorflow as tf
 
 from scipy.ndimage.interpolation import map_coordinates
 from scipy.ndimage.filters import gaussian_filter
 from tqdm import tqdm
 
+FLAGS = None
 
-TRAIN_PATH = '../../../dl_data/nucleus/stage1_train/'
+def main(_):
+    train_ids = next(os.walk(FLAGS.train_dir))[1]
 
-new_id_prefix = '_elastic_'
+    print('Getting and resizing train images and masks ... ')
+    sys.stdout.flush()
 
+    for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
+        for aug_count in (1, FLAGS.aug_count):
+            path = FLAGS.train_dir + id_
+            image_ = cv2.imread(path + '/images/' + id_ + '.png')
+            # image_ = cv2.cvtColor(image_, cv2.COLOR_RGBA2GRAY)
+            # imshow(image_)
+            # plt.show()
+            mask_ = cv2.imread(path + '/gt_mask/' + id_ + '.png')
+            # imshow(mask_)
+            # plt.show()
 
-# Get train and test IDs
-train_ids = next(os.walk(TRAIN_PATH))[1]
+            image = np.concatenate((image_, mask_), axis=2)
 
-print('Getting and resizing train images and masks ... ')
-sys.stdout.flush()
+            alpha = image.shape[1] * 2
+            sigma = image.shape[1] * 0.08
+            alpha_affine = image.shape[1] * 0.05
+            random_state = np.random.RandomState(None)
 
-for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
-    path = TRAIN_PATH + id_
-    image_ = cv2.imread(path + '/images/' + id_ + '.png')
-    # image_ = cv2.cvtColor(image_, cv2.COLOR_RGBA2GRAY)
-    # imshow(image_)
-    # plt.show()
-    mask_ = cv2.imread(path + '/gt_mask/' + id_ + '.png')
-    # imshow(mask_)
-    # plt.show()
+            shape = image.shape
+            shape_size = shape[:2]
 
-    image = np.concatenate((image_, mask_), axis=2)
+            # Random affine
+            center_square = np.float32(shape_size) // 2
+            square_size = min(shape_size) // 3
+            pts1 = np.float32(
+                [center_square + square_size, [center_square[0] + square_size, center_square[1] - square_size],
+                 center_square - square_size])
+            pts2 = pts1 + random_state.uniform(-alpha_affine, alpha_affine, size=pts1.shape).astype(np.float32)
+            M = cv2.getAffineTransform(pts1, pts2)
+            image = cv2.warpAffine(image, M, shape_size[::-1], borderMode=cv2.BORDER_REFLECT_101)
 
-    alpha = image.shape[1] * 2
-    sigma = image.shape[1] * 0.08
-    alpha_affine = image.shape[1] * 0.05
-    random_state = np.random.RandomState(None)
+            dx = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma) * alpha
+            dy = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma) * alpha
+            dz = np.zeros_like(dx)
 
-    shape = image.shape
-    shape_size = shape[:2]
+            x, y, z = np.meshgrid(np.arange(shape[1]), np.arange(shape[0]), np.arange(shape[2]))
+            indices = np.reshape(y + dy, (-1, 1)), np.reshape(x + dx, (-1, 1)), np.reshape(z, (-1, 1))
 
-    # Random affine
-    center_square = np.float32(shape_size) // 2
-    square_size = min(shape_size) // 3
-    pts1 = np.float32([center_square + square_size, [center_square[0] + square_size, center_square[1] - square_size],
-                       center_square - square_size])
-    pts2 = pts1 + random_state.uniform(-alpha_affine, alpha_affine, size=pts1.shape).astype(np.float32)
-    M = cv2.getAffineTransform(pts1, pts2)
-    image = cv2.warpAffine(image, M, shape_size[::-1], borderMode=cv2.BORDER_REFLECT_101)
+            im_merge_t = map_coordinates(image, indices, order=1, mode='reflect').reshape(shape)
 
-    dx = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma) * alpha
-    dy = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma) * alpha
-    dz = np.zeros_like(dx)
+            im_t = im_merge_t[..., 0:3]
+            # imshow(im_t)
+            # plt.show()
+            mask_t = im_merge_t[..., 3:6]
+            mask_t = cv2.cvtColor(mask_t, cv2.COLOR_RGB2GRAY)
+            # imshow(mask_t)
+            # plt.show()
 
-    x, y, z = np.meshgrid(np.arange(shape[1]), np.arange(shape[0]), np.arange(shape[2]))
-    indices = np.reshape(y + dy, (-1, 1)), np.reshape(x + dx, (-1, 1)), np.reshape(z, (-1, 1))
+            randomString = str(uuid.uuid4()).replace("-", "")
 
+            new_id = FLAGS.aug_prefix + randomString + id_[39:]
+            os.mkdir(FLAGS.train_dir + new_id)
+            os.mkdir(FLAGS.train_dir + new_id + '/images/')
+            os.mkdir(FLAGS.train_dir + new_id + '/gt_mask/')
+            cv2.imwrite(FLAGS.train_dir + new_id + '/images/' + new_id + '.png', im_t)
+            cv2.imwrite(FLAGS.train_dir + new_id + '/gt_mask/' + new_id + '.png', mask_t)
 
-    im_merge_t = map_coordinates(image, indices, order=1, mode='reflect').reshape(shape)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--train_dir',
+        default='../../../dl_data/nucleus/stage1_train/',
+        type=str,
+        help="Train Data directory")
 
-    # im_t = map_coordinates(image, indices, order=1, mode='reflect').reshape(shape)
-    # mask_t = map_coordinates(mask_, indices, order=1, mode='reflect').reshape(shape)
+    parser.add_argument(
+        '--aug_prefix',
+        default='_elastic_',
+        type=str,
+        help="prefix name of augmentation")
 
-    im_t = im_merge_t[...,0:3]
-    # imshow(im_t)
-    # plt.show()
-    mask_t = im_merge_t[...,3:6]
-    mask_t = cv2.cvtColor(mask_t, cv2.COLOR_RGB2GRAY)
-    # imshow(mask_t)
-    # plt.show()
+    parser.add_argument(
+        '--aug_count',
+        type=int,
+        default=2,
+        help="Count of augmentation")
 
-    randomString = str(uuid.uuid4()).replace("-", "")
-
-    new_id = new_id_prefix + randomString + id_[39:]
-    os.mkdir(TRAIN_PATH + new_id)
-    os.mkdir(TRAIN_PATH + new_id + '/images/')
-    os.mkdir(TRAIN_PATH + new_id + '/gt_mask/')
-    cv2.imwrite(TRAIN_PATH + new_id + '/images/' + new_id + '.png', im_t)
-    cv2.imwrite(TRAIN_PATH + new_id + '/gt_mask/' + new_id + '.png', mask_t)
-
-"""
-    for mask_file in next(os.walk(path + '/masks/'))[2]:
-        mask_ = cv2.imread(path + '/masks/' + mask_file, -1)
-        # change from GRAY to RGBA
-        mask_ = cv2.cvtColor(mask_, cv2.COLOR_GRAY2RGBA)
-        mask_t = map_coordinates(mask_, indices, order=1, mode='reflect').reshape(shape)
-        mask_t = cv2.cvtColor(mask_t, cv2.COLOR_RGBA2GRAY)
-        new_mask_id = 'elastic' + mask_file[7:]
-        cv2.imwrite(TRAIN_PATH + new_id + '/masks/' + new_mask_id + '.png', mask_t)
-"""
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
