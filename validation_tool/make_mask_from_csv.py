a9e327d77c56243603aa2a92edbc16ad0bcf89b4
ziippy <ziippy@naver.com>
2018-04-08 00:44:33 +0900

    mask_dir, gt_mask_dir

--- validation_tool/make_mask_from_csv.py
+++ validation_tool/make_mask_from_csv.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 import argparse
 import os
 import sys
+import shutil
 
 import pandas as pd
 
@@ -22,13 +23,17 @@ def get_image_size(imageId):
 
     return img.height, img.width
 
+def remove_eval_dir(imageId, target_path):
+    dir_path = os.path.join(FLAGS.dataset_dir, imageId, target_path)
+    if os.path.exists(dir_path):
+        shutil.rmtree(dir_path)
 
-def save_to_image(imageId, data):
-    mask_path = os.path.join(FLAGS.dataset_dir, imageId, FLAGS.mask_dir)
+def save_to_image(imageId, data, target_path, prefix):
+    mask_path = os.path.join(FLAGS.dataset_dir, imageId, target_path)
     if not os.path.exists(mask_path):
         os.makedirs(mask_path)
 
-    target = os.path.join(mask_path, imageId + '.png')
+    target = os.path.join(mask_path, prefix + imageId + '.png')
 
     # Rescale to 0-255 and convert to uint8
     rescaled = (255.0 / data.max() * (data - data.min())).astype(np.uint8)
@@ -36,7 +41,7 @@ def save_to_image(imageId, data):
     # save
     img = Image.fromarray(rescaled)
     img.save(target)
-    print("save_to_image >>> ", imageId)
+    print("save_to_image >>>", mask_path)
 
 
 def main(_):
@@ -53,6 +58,7 @@ def main(_):
     image = np.zeros(0)
     height = 0
     width = 0
+    per_image_index = 0
     for imageId in data_frame['ImageId']:
         #print(imageId)
 
@@ -63,10 +69,13 @@ def main(_):
                 #plt.imshow(image_2, cm.gray)
                 #plt.show()
                 # save
-                save_to_image(preImageId, image_2)
+                save_to_image(preImageId, image_2, FLAGS.gt_mask_dir, '')
 
             preImageId = imageId
             imageIdChanged = True
+            per_image_index = 0
+            remove_eval_dir(imageId, FLAGS.mask_dir)
+            remove_eval_dir(imageId, FLAGS.gt_mask_dir)
         else:
             imageIdChanged = False
 
@@ -78,6 +87,7 @@ def main(_):
         #print(mask_info)
         #print(mask_array)
 
+        per_image = np.zeros((height, width), dtype=np.uint8).flatten()
         if imageIdChanged == True:
             image = np.zeros((height, width), dtype=np.uint8).flatten()
         for i in range(0, len(mask_array), 2):
@@ -86,12 +96,17 @@ def main(_):
             #print("start >> ", start_pos)
             #print("mask_length >> ", mask_length)
             for j in range(mask_length):
+                per_image[start_pos - 1 + j] = 1
                 image[start_pos - 1 + j] = 1
 
         # show per each
-        #image_2 = image.reshape(width, height).T
-        #plt.imshow(image_2, cm.gray)
+        per_image_2 = per_image.reshape(width, height).T
+        #plt.imshow(per_image, cm.gray)
         #plt.show()
+
+        # save
+        save_to_image(preImageId, per_image_2, FLAGS.mask_dir, str(per_image_index))
+        per_image_index += 1
 
         index += 1
         #break
@@ -101,20 +116,22 @@ def main(_):
         #plt.imshow(image_2, cm.gray)
         #plt.show()
         # save
-        save_to_image(preImageId, image_2)
+        save_to_image(preImageId, image_2, FLAGS.gt_mask_dir, '')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--labels_path',
-        default='../../../dev-repo/2018-Data-Science-Bowl/result/submission-nucleus_det-20.csv',
+        default='../result/submission-nucleus_det-100.csv',
+        # default='../../../dl_data/nucleus/stage1_train_labels/stage1_train_labels.csv',
         type=str,
         help="Data directory")
 
     parser.add_argument(
         '--dataset_dir',
         default='../../../dl_data/nucleus/stage1_test',
+        # default='../../../dl_data/nucleus/stage1_train',
         type=str,
         help="Labels directory")
 
@@ -123,6 +140,12 @@ if __name__ == '__main__':
         default='eval_mask',
         type=str,
         help="mask directory name")
+
+    parser.add_argument(
+        '--gt_mask_dir',
+        default='eval_gt_mask',
+        type=str,
+        help="gt_mask directory name")
 
     FLAGS, unparsed = parser.parse_known_args()
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
