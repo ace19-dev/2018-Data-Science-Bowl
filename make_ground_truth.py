db7cd60c21692a23b30c85b16bb291901a673ed5
ziippy <ziippy@naver.com>
2018-04-11 00:54:48 +0900

    use_countour argument add

--- make_ground_truth.py
+++ make_ground_truth.py
@@ -12,6 +12,10 @@ from skimage.io import imread, imshow
 
 from utils.image_utils import read_image
 
+import tqdm
+import glob
+import cv2
+import matplotlib.pyplot as plt
 
 # def get_image_size(data):
 #     image_path = os.path.join(FLAGS.dataset_dir, data, 'images')
@@ -20,6 +24,31 @@ from utils.image_utils import read_image
 #
 #     return img.height, img.width
 
+def get_contour(img):
+    img_contour = np.zeros_like(img).astype(np.uint8)
+    _, contours, hierarchy = cv2.findContours(img.astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(img_contour, contours, -1, (255, 255, 255), 1)
+    return img_contour
+
+def overlay_contours(images_dir, subdir_name, target_dir, touching_only=False):
+    train_dir = os.path.join(images_dir, subdir_name)
+    contours = []
+    for mask_dirname in tqdm(glob.glob('{}/*/masks'.format(train_dir))):
+        masks = []
+        for image_filepath in glob.glob('{}/*'.format(mask_dirname)):
+            image = np.asarray(Image.open(image_filepath))
+            image = image / 255.0
+            masks.append(get_contour(image))
+        if touching_only:
+            overlayed_masks = np.where(np.sum(masks, axis=0) > 128. + 255., 255., 0.).astype(np.uint8)
+        else:
+            overlayed_masks = np.where(np.sum(masks, axis=0) > 128., 255., 0.).astype(np.uint8)
+        #target_filepath = '/'.join(mask_dirname.replace(images_dir, target_dir).split('/')[:-1]) + '.png'
+        #os.makedirs(os.path.dirname(target_filepath), exist_ok=True)
+        #imwrite(target_filepath, overlayed_masks)
+        contours.append(overlayed_masks)
+
+    return contours
 
 def main(_):
 
@@ -31,17 +60,35 @@ def main(_):
         mask_path = os.path.join(FLAGS.dataset_dir, data, 'masks')
         mask_images = sorted(os.listdir(mask_path))
         mask = np.zeros((img_shape[0], img_shape[1], 1), dtype=np.bool)
+        if FLAGS.use_countour:
+            countour = np.zeros((img_shape[0], img_shape[1], 1), dtype=np.bool)
         for mask_file in mask_images:
             _mask = imread(os.path.join(mask_path, mask_file))
             _mask = np.expand_dims(_mask, axis=-1)
             mask = np.maximum(mask, _mask)
+            #
+            if FLAGS.use_countour:
+                _countour = get_contour(_mask)
+                countour = np.maximum(countour, _countour)
+                #imshow(np.squeeze(_countour))
+                #plt.show()
 
-        gt_path = os.path.join(FLAGS.ground_truth_dir, data, 'gt_mask')
+        gt_path = os.path.join(FLAGS.ground_truth_dir, data, FLAGS.ground_truth_folder)
         if not os.path.exists(gt_path):
             os.makedirs(gt_path)
 
-        # imshow(np.squeeze(mask))
-        # plt.show()
+        print(data)
+
+        #imshow(np.squeeze(countour))
+        #plt.show()
+
+        #imshow(np.squeeze(mask))
+        #plt.show()
+
+        if FLAGS.use_countour:
+            mask = mask - countour
+        #imshow(np.squeeze(mask))
+        #plt.show()
 
         mask = np.squeeze(mask)
         img = Image.fromarray(mask)
@@ -62,6 +109,18 @@ if __name__ == '__main__':
         default='../../dl_data/nucleus/stage1_train',
         type=str,
         help="ground_truth data directory")
+
+    parser.add_argument(
+        '--ground_truth_folder',
+        default='gt_mask2',
+        type=str,
+        help="ground_truth folder")
+
+    parser.add_argument(
+        '--use_countour',
+        default=True,
+        type=bool,
+        help="use countour")
 
     FLAGS, unparsed = parser.parse_known_args()
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
